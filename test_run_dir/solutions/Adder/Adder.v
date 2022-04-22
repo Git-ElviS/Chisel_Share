311c33886be1dcd77b37d5c63470ff18a9c67358
蔡忠軒 <caizhongxuan@caizhongxuande-MacBook-Air.local>
2022-04-22 11:12:47 +0800

    [clean] remove the tempprary files

--- test_run_dir/solutions/Adder/Adder.v
+++
@@ -1,9 +0,0 @@
-module Adder(
-  input        clock,
-  input        reset,
-  input  [7:0] io_in0,
-  input  [7:0] io_in1,
-  output [7:0] io_out
-);
-  assign io_out = io_in0 + io_in1; // @[Adder.scala 17:10]
-endmodule
